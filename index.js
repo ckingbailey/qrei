a687eae90ce34ddb0da4393b69fc32c4b87dec51
ckb <ckingbailey@gmail.com>
2024-09-16 21:58:49 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import fetch from 'node-fetch';
+import { JSDOM } from 'jsdom';
+
+const base_url = 'https://rei.com/search'
+const qs = 'q=approach+shoes?&ir=q:approach+shoes&r=category:mens-footwear;deals:See+All+Deals';
+
+const response = await fetch(`${base_url}?${encodeURIComponent(qs)}`, {
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion'
+    }
+});
+const body = await response.text();
+
+const dom = new JSDOM(body);
+
+const results = dom.window.document.getElementById('search-results');
+console.log(results);
