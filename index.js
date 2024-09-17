a405a847dccdf696b06bc61ae39da2d83f5e66b9
ckb <ckingbailey@gmail.com>
2024-09-16 22:06:13 -0700

    comments with plans

--- index.js
+++ index.js
@@ -14,4 +14,9 @@ const body = await response.text();
 const dom = new JSDOM(body);
 
 const results = dom.window.document.getElementById('search-results');
+
+// get children of div#search-results
+// get first <ul> from children
+// get children of <ul>
+// get all <li> children of <ul>
 console.log(results);
