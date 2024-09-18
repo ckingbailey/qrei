68ad587a924f09c671d9f89338a5f530b0a6b22e
colin-mbp-2022 <colin@spartascience.com>
2024-09-17 17:07:53 -0700

    get list of textContent and href

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
+/*
+In general:
+    • Search rei website using query string
+        • Need query string class to assemble their peculiar qs
+    • Parse results for product name, price, link
+    • Send notification with list of products, prices, links
+*/
 import fetch from 'node-fetch';
 import { JSDOM } from 'jsdom';
 
 const base_url = 'https://rei.com/search'
-const qs = 'q=approach+shoes?&ir=q:approach+shoes&r=category:mens-footwear;deals:See+All+Deals';
+const filters = [
+    [ 'gender', 'Men\'s' ],
+    [ 'size', 10 ],
+    [ 'deals', 'See+All+Deals' ]
+];
+const query = {
+    q: 'approach+shoes',
+    r: filters.map(f => f.join(encodeURIComponent(':'))).join(encodeURIComponent(';'))
+};
+const qs = Object.entries(query).map(q => q.join('=')).join('&');
+console.log(qs)
 
-const response = await fetch(`${base_url}?${encodeURIComponent(qs)}`, {
+const url = `${base_url}?${qs}`;
+console.log(url)
+const response = await fetch(url, {
     headers: {
         'User-Agent': 'Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion'
     }
 });
-const body = await response.text();
 
+if (response.status !== 200) {
+    throw Error(`${response.status}: ${response.statusText}`)
+}
+
+const body = await response.text();
 const dom = new JSDOM(body);
 
 const results = dom.window.document.getElementById('search-results');
@@ -19,4 +42,35 @@ const results = dom.window.document.getElementById('search-results');
 // get first <ul> from children
 // get children of <ul>
 // get all <li> children of <ul>
-console.log(results);
+
+// search for search results unordered list
+let uls = [];
+console.log('num results', results.children.length);
+for (const childItem of results.children) {
+    if (childItem.tagName.toLowerCase() === 'ul') {
+        uls.push(childItem);
+    }
+}
+
+// search of list items in first ul
+let lis = [];
+console.log('num children of ul 0', uls[0].children.length);
+for (const childItem of uls[0].children) {
+    if (childItem.tagName.toLowerCase() === 'li') {
+        // Is there any advantage of children (HTMLCollection) vs. childNodes (NodeList)?
+        // What is the difference in behavior of the 2 kinds of collections?
+        const itemText = childItem.textContent;
+        const links = new Set();
+        for (const innerChild of childItem.children) {
+            if (innerChild.tagName.toLowerCase() === 'a') {
+                links.add(innerChild.href);
+            }
+        }
+        if (links.size > 1) {
+            console.warn(`Found more than 1 unique link for item ${itemText}`, links);
+        }
+        lis.push([ itemText, links.values().next().value ]);
+    }
+}
+
+console.log(lis);
