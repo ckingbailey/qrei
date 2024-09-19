40289c6454d7e7487979fba1eaf057836f137ae8
ckb <ckingbailey@gmail.com>
2024-09-18 19:16:56 -0700

    scraper class

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /*
 In general:
-    • Search rei website using query string
-        • Need query string class to assemble their peculiar qs
-    • Parse results for product name, price, link
-    • Send notification with list of products, prices, links
+    - [x] Search rei website using query string
+        - [x] Need query string class to assemble their peculiar qs
+    - [] Parse results for product name, price, link
+    - [] Send notification with list of products, prices, links
 */
 import fetch from 'node-fetch';
 import { JSDOM } from 'jsdom';
@@ -59,9 +59,34 @@ class ReiClient {
     }
 }
 
+class Scraper {
+    constructor(html, classname) {
+        this.dom = new JSDOM(html);
+        this.searchResultsClassname = classname || 'search-results';
+    }
+
+    matchTag(tag) {
+        return (element) => element.tagName.toLowerCase() == tag;
+    }
+
+    getResultsList() {
+        const results = this.dom.window.document.getElementById(this.searchResultsClassname);
+        const hits = Array.from(Array.from(results.children).filter(this.matchTag('ul'))[0]
+            .children).filter(this.matchTag('li')).map(li => {
+                const text = li.textContent;
+                const links = new Set(Array.from(li.children).filter(this.matchTag('a')).map(a => a.href));
+                return [
+                    text,
+                    links.values().next().value
+                ];
+            })
+        return hits;
+    }
+}
+
 const filters = [
     [ 'gender', 'Men\'s' ],
-    [ 'size', 10 ],
+    // [ 'size', 10 ],
     [ 'deals', 'See+All+Deals' ]
 ];
 const query = new Query('approach+shoes', new QueryFilter(...filters));
@@ -72,43 +97,8 @@ const rei = new ReiClient();
 const response = await rei.search(query);
 
 const body = await response.text();
-const dom = new JSDOM(body);
 
-const results = dom.window.document.getElementById('search-results');
+const scraper = new Scraper(body)
+const results = scraper.getResultsList()
 
-// get children of div#search-results
-// get first <ul> from children
-// get children of <ul>
-// get all <li> children of <ul>
-
-// search for search results unordered list
-let uls = [];
-console.log('num results', results.children.length);
-for (const childItem of results.children) {
-    if (childItem.tagName.toLowerCase() === 'ul') {
-        uls.push(childItem);
-    }
-}
-
-// search of list items in first ul
-let lis = [];
-console.log('num children of ul 0', uls[0].children.length);
-for (const childItem of uls[0].children) {
-    if (childItem.tagName.toLowerCase() === 'li') {
-        // Is there any advantage of children (HTMLCollection) vs. childNodes (NodeList)?
-        // What is the difference in behavior of the 2 kinds of collections?
-        const itemText = childItem.textContent;
-        const links = new Set();
-        for (const innerChild of childItem.children) {
-            if (innerChild.tagName.toLowerCase() === 'a') {
-                links.add(innerChild.href);
-            }
-        }
-        if (links.size > 1) {
-            console.warn(`Found more than 1 unique link for item ${itemText}`, links);
-        }
-        lis.push([ itemText, links.values().next().value ]);
-    }
-}
-
-console.log(lis);
+console.log(results);
